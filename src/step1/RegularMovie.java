cc194aad76e49e98cf941f74dd21b8c8900351b8
Pablo <dongsik2026@naver.com>
2018-05-03 17:55:46 +0900

    [구현코드리팩토링]RegularMovie 클레스를 추가

---
+++ src/step1/RegularMovie.java
@@ -0,0 +1,17 @@
+package step1;
+
+public class RegularMovie extends Movie {
+
+	public RegularMovie(String title) {
+		super(title, Movie.REGULAR);
+	}
+
+	@Override
+	double getChargeFor(int daysRented) {
+		double thisAmount = 0;
+		thisAmount += 2;
+		if (daysRented > 2)
+			thisAmount += (daysRented - 2) * 1.5;
+		return thisAmount;
+	}
+}
