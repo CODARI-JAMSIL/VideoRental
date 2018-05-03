38b07bedd28e6d414ceb5e563c75aa88fa4dfa76
Pablo <dongsik2026@naver.com>
2018-05-03 13:11:03 +0900

    [테스트코드작성]고객 생성 테스트코드 작성

--- test/step1/CustomerTest.java
+++ test/step1/CustomerTest.java
@@ -1,9 +1,21 @@
 package step1;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class CustomerTest {
 	
+	@Test
+	public void 绊按_积己(){
+		//GIVEN
+		Customer customer = new Customer("绊按捞抚");
+		
+		//WHEN
+		
+		//THEN
+		assertThat(customer, is(notNullValue()));
+	}
 }
