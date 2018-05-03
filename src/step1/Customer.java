0f2b87bc13a18c37e04df75a1fefa293ee8542ff
Pablo <dongsik2026@naver.com>
2018-05-03 12:46:04 +0900

    git init

---
+++ src/step1/Customer.java
@@ -0,0 +1,70 @@
+package step1;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+class Customer {
+	private String name;
+	private List<Rental> rentals = new ArrayList<>();
+
+	public Customer(String name) {
+		this.name = name;
+	};
+
+	public void addRental(Rental rental) {
+		rentals.add(rental);
+	}
+
+	public String getName() {
+		return name;
+	};
+
+	public String statement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		Iterator<Rental> iterator = rentals.iterator();
+		String result = "Rental Record for " + getName() + "\n";
+
+		while ( iterator.hasNext() ) {
+			double thisAmount = 0;
+			Rental each = (Rental) iterator.next();
+			// determine amounts for each line
+
+			switch (each.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (each.getDaysRented() > 2)
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				break;
+				
+			case Movie.NEW_RELEASE:
+				thisAmount += each.getDaysRented() * 3;
+				break;
+				
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (each.getDaysRented() > 3)
+					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				break;
+			}
+
+			// add frequent renter points
+			frequentRenterPoints++;
+			// add bonus for a two day new release rental
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+				frequentRenterPoints++;
+			// show figures
+			result += "\t" +  String.valueOf(thisAmount) + "(" + each.getMovie().getTitle() + ")" + "\n";
+
+			totalAmount += thisAmount;
+		}
+
+		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter pointers";
+
+		return result;
+	}
+}
---
+++ test/step1/CustomerTest.java
@@ -0,0 +1,9 @@
+package step1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerTest {
+	
+}
