c848f89c5e560033e947d70e61df332ba6523e52
Pablo <dongsik2026@naver.com>
2018-05-03 13:26:33 +0900

    [테스트코드작성]regularMovie2일대여하는 경우의 테스트코드 작성

--- test/step1/CustomerTest.java
+++ test/step1/CustomerTest.java
@@ -33,4 +33,26 @@ public class CustomerTest {
 				));
 		
 	}
+
+	@Test
+	public void regularMovie2일대여(){
+		//GIVEN
+		Customer customer = new Customer("고객이름");
+		Movie movie = new Movie("RegularMovie", Movie.REGULAR);
+		int daysRented = 2;
+		Rental rental = new Rental(movie, daysRented);
+		customer.addRental(rental);
+		
+		//WHEN
+		String statement = customer.statement();
+		
+		//THEN
+		assertThat(statement, is("Rental Record for 고객이름\n"
+		        + "\t2.0(RegularMovie)\n" 
+				+"Amount owed is 2.0\n"
+				+"You earned 1 frequent renter pointers"
+				));
+		
+	}
+
 }
