34a1cab65f7a5dc8780d648e1b93380dd97f78af
Pablo <dongsik2026@naver.com>
2018-05-03 17:38:26 +0900

    [구현코드리팩토링]getChargeFor를 Movie Class로 이동하고 코드 수정

--- src/step1/Movie.java
+++ src/step1/Movie.java
@@ -23,5 +23,27 @@ public class Movie {
 
 	public String getTitle() {
 		return title;
+	}
+
+	double getChargeFor(int daysRented) {
+		double thisAmount = 0;
+		switch (getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (daysRented > 2)
+				thisAmount += (daysRented - 2) * 1.5;
+			break;
+			
+		case Movie.NEW_RELEASE:
+			thisAmount += daysRented * 3;
+			break;
+			
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (daysRented > 3)
+				thisAmount += (daysRented - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
 	};
 }
--- src/step1/Rental.java
+++ src/step1/Rental.java
@@ -18,29 +18,7 @@ class Rental {
 	}
 
 	public double getCharge() {
-		return getChargeFor(daysRented);
-	}
-
-	private double getChargeFor(int daysRented) {
-		double thisAmount = 0;
-		switch (getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (getDaysRented() > 2)
-				thisAmount += (daysRented - 2) * 1.5;
-			break;
-			
-		case Movie.NEW_RELEASE:
-			thisAmount += daysRented * 3;
-			break;
-			
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (daysRented > 3)
-				thisAmount += (daysRented - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
+		return movie.getChargeFor(daysRented);
 	}
 
 	int frequentRentersFor() {
