3f6ceaf59806fbc50a21bce8b631e130be044ba0
Pablo <dongsik2026@naver.com>
2018-05-04 16:41:38 +0900

    [구현코드리팩토링]Movie 클래스를 추상화처리하고 getChargeFor 메서드를 추상화처리함

--- src/step1/Movie.java
+++ src/step1/Movie.java
@@ -1,6 +1,6 @@
 package step1;
 
-public class Movie {
+public abstract class Movie {
 	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
@@ -25,27 +25,7 @@ public class Movie {
 		return title;
 	}
 
-	double getChargeFor(int daysRented) {
-		double thisAmount = 0;
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (daysRented > 2)
-				thisAmount += (daysRented - 2) * 1.5;
-			break;
-			
-		case Movie.NEW_RELEASE:
-			thisAmount += daysRented * 3;
-			break;
-			
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (daysRented > 3)
-				thisAmount += (daysRented - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
-	}
+	abstract double getChargeFor(int daysRented);
 
 	public int frequentRentersFor(int daysRented) {
 		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
