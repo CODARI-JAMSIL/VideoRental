c4f4e88a5042442502ca739502365658acdb6ea3
nubang <nubang@nubang>
2018-06-19 01:57:41 +0900

    switch문 들여쓰기

--- src/step1/Customer.java
+++ src/step1/Customer.java
@@ -34,23 +34,23 @@ class Customer {
 			Rental each = (Rental) iterator.next();
 
 			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2) {
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				}
-				break;
-				
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-				
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3) {
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				}
-				break;
+				case Movie.REGULAR:
+					thisAmount += 2;
+					if (each.getDaysRented() > 2) {
+						thisAmount += (each.getDaysRented() - 2) * 1.5;
+					}
+					break;
+					
+				case Movie.NEW_RELEASE:
+					thisAmount += each.getDaysRented() * 3;
+					break;
+					
+				case Movie.CHILDRENS:
+					thisAmount += 1.5;
+					if (each.getDaysRented() > 3) {
+						thisAmount += (each.getDaysRented() - 3) * 1.5;
+					}
+					break;
 			}
 
 			frequentRenterPoints++;
